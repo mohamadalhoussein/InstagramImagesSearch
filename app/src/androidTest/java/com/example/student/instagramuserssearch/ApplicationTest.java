daf2b98b71e21698cb73d46e13208bbac518c6a5
student <student@UPP000056519P.win2k.aub.edu.lb>
2017-10-01 15:07:21 +0300

    second commit

---
+++ app/src/androidTest/java/com/example/student/instagramuserssearch/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.student.instagramuserssearch;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by student on 10/1/2017.
+ */
+
+public class ApplicationTest extends ApplicationTestCase<Application>{
+    public ApplicationTest(){
+        super(Application.class);
+    }
+}
